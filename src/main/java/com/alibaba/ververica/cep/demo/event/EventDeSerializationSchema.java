b798aec6d934526507689174f775ada4fbdf676f
Biao Geng <gbvkken@gmail.com>
2025-02-10 11:05:55 +0800

    Update to Flink 1.17.2 and VVR8.0.8

--- src/main/java/com/alibaba/ververica/cep/demo/event/EventDeSerializationSchema.java
+++ src/main/java/com/alibaba/ververica/cep/demo/event/EventDeSerializationSchema.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KTD, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.ververica.cep.demo.event;
 
 import org.apache.flink.api.common.serialization.DeserializationSchema;
@@ -7,8 +25,7 @@ import org.apache.flink.util.Collector;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.io.IOException;
-
+/** KafkaRecordDeserializationSchema for Event. */
 public class EventDeSerializationSchema implements KafkaRecordDeserializationSchema<Event> {
 
     private static final long serialVersionUID = 6154188370181669758L;
@@ -25,8 +42,7 @@ public class EventDeSerializationSchema implements KafkaRecordDeserializationSch
 
     @Override
     public void deserialize(
-            ConsumerRecord<byte[], byte[]> consumerRecord, Collector<Event> collector)
-            throws IOException {
+            ConsumerRecord<byte[], byte[]> consumerRecord, Collector<Event> collector) {
         collector.collect(Event.fromString(new String(consumerRecord.value())));
     }
 }
